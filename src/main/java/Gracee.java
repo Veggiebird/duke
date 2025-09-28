9e925d7df3dcc0493204a8a78e0d09c7f17bbfd0
veggiebird <graceooi1028@gmail.com>
2025-09-28 20:01:38 +0800

    level-0, level-1, level-2, level-3, level-4

--- src/main/java/Gracee.java
+++ src/main/java/Gracee.java
@@ -27,7 +27,7 @@ public class Gracee {
             String inputMain = sc.nextLine();
 
             switch(inputMain){
-                case "1":
+                case "1": /* Main menu: 1. Shop business hour and details.*/
                     System.out.println("___________________________");
                     System.out.println("Opening time: 10:00 AM - 17:00 PM.");
                     System.out.println("Address: 123 Blk Rich, S888999");
@@ -37,7 +37,7 @@ public class Gracee {
                     historical.add("Asked shop details.");
                     break;
 
-                case "2":
+                case "2": /* Main menu: 2. Buying/Selling price of gold.*/
                     System.out.println("___________________________");
                     System.out.println("Today's buying price for gold is: \n 916: 150 SGD per gram\n 999: 180 SGD per gram");
                     System.out.println("Today's selling price for gold is: \n 916: 170 SGD per gram\n 999: 200 SGD per gram");
@@ -45,7 +45,7 @@ public class Gracee {
                     historical.add("Asked gold price.");
                     break;
 
-                case "3":
+                case "3": /* Main menu: 3. List historical conversation.*/
                     System.out.println("___________________________");
                     System.out.println("Previous action: ");
                     for(int i = 0; i < historical.size(); i++){
@@ -54,7 +54,7 @@ public class Gracee {
                     System.out.println("___________________________");
                     break;
 
-                case "4":
+                case "4": /* Main menu: Check your task list.*/
                     System.out.println("___________________________");
                     System.out.println("You are in task list interface.");
 
@@ -72,7 +72,7 @@ public class Gracee {
                        String taskInput = sc.nextLine();
 
                        switch(taskInput){
-                           case "1":
+                           case "1": /*  Task menu: 1. Add task.  */
 
                                gracee_task_list newTask = null;
 
@@ -105,7 +105,7 @@ public class Gracee {
                                System.out.println("Now you have " + task_list.size() + " tasks in the list.");
                                break;
 
-                           case "2":
+                           case "2": /*  Task menu: 2. Update task (Update to Done or Pending)  */
 
                                if(task_list.isEmpty()){
                                    System.out.println("No task available.");
@@ -117,34 +117,36 @@ public class Gracee {
                                    System.out.println((i+1)+": " + task_list.get(i));
                                }
                                System.out.println("Please enter task number that you would like to modify.");
-                               int task_number = Integer.parseInt(sc.nextLine());
-                               int index = task_number - 1;
 
-                               if(index < 0 || index >= task_list.size()){
-                                   System.out.println("Invalid task number.");
+                               try {
+                                   int task_number = Integer.parseInt(sc.nextLine().trim());
+                                   int index = task_number - 1;
+
+                                   System.out.println((task_number)+ ": " + task_list.get(index));
+                                   if(task_list.get(index).getStatus().equals("Done")){
+                                       task_list.get(index).markAsPending();
+                                       System.out.println((task_number)+": " + task_list.get(index) + " has been marked as Pending.");
+                                   }
+                                   else if(task_list.get(index).getStatus().equals("Pending")){
+                                       task_list.get(index).markAsDone();
+                                       System.out.println((task_number)+": " + task_list.get(index) + " has been marked as Done.");
+                                   }
+                               } catch (NumberFormatException e) {
+                                   System.out.println("You did not enter a number. Please enter number and not other characters.");
                                    break;
+                               } catch (IndexOutOfBoundsException e) {
+                                   System.out.println("The task number is invalid, please enter valid task number between 1 - " + task_list.size());
                                }
-
-                               System.out.println((task_number)+": " + task_list.get(index));
-                               if(task_list.get(index).getStatus().equals("Done")){
-                                   task_list.get(index).markAsPending();
-                                   System.out.println((task_number)+": " + task_list.get(index) + " has been marked as Pending.");
-                               }
-                               else if(task_list.get(index).getStatus().equals("Pending")){
-                                   task_list.get(index).markAsDone();
-                                   System.out.println((task_number)+": " + task_list.get(index) + " has been marked as Done.");
-                               }
-
                                break;
 
-                           case "3":
+                           case "3": /* Task menu: 3. List tasks.*/
                                System.out.println("Task list as below");
                                for(int i = 0; i < task_list.size(); i++){
                                    System.out.println((i+1)+": " + task_list.get(i));
                                }
                                break;
 
-                           case "4":
+                           case "4": /* Task menu: 4. Remove task*/
                                if(task_list.isEmpty()){
                                    System.out.println("No task available.");
                                    break;
@@ -154,20 +156,24 @@ public class Gracee {
                                for(int i = 0; i < task_list.size(); i++){
                                    System.out.println((i+1)+": " + task_list.get(i));
                                }
-                               System.out.println("Please enter task number that you would like to modify.");
-                               int rm_task_number = Integer.parseInt(sc.nextLine());
-                               int rm_index = rm_task_number - 1;
+                               System.out.println("Please enter task number that you would like to remove.");
 
-                               if(rm_index < 0 || rm_index >= task_list.size()){
-                                   System.out.println("Invalid task number.");
+                               try{
+                                    int rm_task_number = Integer.parseInt(sc.nextLine());
+                                    int rm_index = rm_task_number - 1;
+
+                                    System.out.println((rm_task_number)+": " + task_list.get(rm_index) + " is removed.");
+                                    task_list.remove(rm_index);
+                               } catch (NumberFormatException e) {
+                                   System.out.println("You did not enter a number. Please enter number and not other characters.");
                                    break;
-                               }else{
-                                   System.out.println((rm_task_number)+": " + task_list.get(rm_index) + " is removed.");
-                                   task_list.remove(rm_index);
+                               } catch (IndexOutOfBoundsException e) {
+                                   System.out.println("The task number is invalid, please enter valid task number between 1 - " + task_list.size());
+                                   break;
                                }
                                break;
 
-                           case "5":
+                           case "5": /* Task menu: 5. Exit task list interface.*/
                                System.out.println("Back to main menu.");
                                taskMode = false;
                                break;
@@ -178,13 +184,15 @@ public class Gracee {
                     }
                 break;
 
-                case "5":
+                case "5": /* Main menu: 5. Bye*/
                     System.out.println("Thank you, Bye");
                     chatLive = false;
                     break;
+
+                default:
+                    System.out.println("Invalid option. Please enter 1-5.");
+                    break;
             }
-
-
         }
         sc.close();
     }
